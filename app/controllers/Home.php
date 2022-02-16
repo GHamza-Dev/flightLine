cf0ee8952635586df505ed33daa201d55457e099
ghamza-dev <elgassaihamzam54@gmail.com>
2022-02-16 23:01:57 +0100

    Create selectUsers and selectUserById methods

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -21,6 +21,23 @@ class Home extends Controller{
 
     public function addUser(){
         $user = $this->getModelInstance('user');
-        $user->insertUser('HH123456','Hamza','Gassai','067398737','0000');
+        // $user->insertUser('HH123456','Hamza','Gassai','067398737','0000');
+        // $user->insertUser(['BB123456','Yassine','Lamine','067398737','0000']);
+        // $user->insertUser(['CC123456','Aymen','Traw','067398737','0000']);
+        // $user->insertUser(['DD123456','Rihab','TAwil','067398737','0000']);
+        echo 'Go to line ['.__LINE__.'] and complete the method :)';
+    }
+
+    function userExist(){
+        echo '<pre>';
+        $user = $this->getModelInstance('user');
+        if ($u = $user->selectUserById(9)) {
+            dump($u);
+        }else echo 'User does not exist!';
+    }
+
+    function getUsers(){
+        $user = $this->getModelInstance('user');
+        dump($user->selectUsers());
     }
 }
--- app/models/User.php
+++ app/models/User.php
@@ -3,16 +3,29 @@
 
 class User extends BaseModel{
 
-    public function __construct($tableName = 'user',$parimaryKey = 'userID'){
-        parent::__construct($tableName,$parimaryKey);
+    public function __construct($tableName = 'user',$primaryKey = 'userID'){
+        parent::__construct($tableName,$primaryKey);
     }
 
-    public function insertUser($nic,$firstName,$lastName,$phone,$password){
-        return $this->db->insert($this->table,
-            ['nic','firstName','lastName','phone','password'],
-            [$nic,$firstName,$lastName,$phone,$password]
-        );
+    public function insertUser($data){
+        $params = [
+            'nic'=> $data[0],
+            'firstName'=> $data[1],
+            'lastName'=> $data[2],
+            'phone'=> $data[3],
+            'password' => $data[4]
+        ];
+        return $this->db->insert($this->table,$params);
     }
 
+    public function selectUserById($id){
+        $this->db->prepareQuery("SELECT * FROM $this->table WHERE $this->primaryKey = ?");
+        return $this->db->execute([$id]) ? $this->db->getRow() : false;
+        
+    }
+
+    public function selectUsers(){
+        return $this->db->select($this->table);
+    }
 
 }
