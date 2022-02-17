fbb054788c9188307f2c9f2ad91d1eb0d06742e4
ghamza-dev <elgassaihamzam54@gmail.com>
2022-02-17 20:38:48 +0100

    Create updateFlight method

--- app/models/Flight.php
+++ app/models/Flight.php
@@ -19,4 +19,17 @@ class Flight extends BaseModel{
         return $this->db->insert($this->table,$params);
     }
 
+    public function updateFlight($data,$id){
+        $params = [
+            'aFrom'=> $data[0],
+            'aTo'=> $data[1],
+            'departTime'=> $data[2],
+            'arrivalTime'=> $data[3],
+            'price' => $data[4],
+            'nbrPlaces'=> $data[5],
+            'reservedPlaces'=> $data[6]
+        ];
+        return $this->db->update($this->table,$this->primaryKey,$id,$params);
+    }
+
 }
