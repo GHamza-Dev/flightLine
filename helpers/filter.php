4034db3df83e16b9ae5748d175160e8ccf806407
ghamza-dev <elgassaihamzam54@gmail.com>
2022-02-17 11:32:23 +0100

    Improve filter helper

--- helpers/filter.php
+++ helpers/filter.php
@@ -1,8 +1,15 @@
 <?php
 
 
-function filter($data,$dataType = null){
-    $data = trim($data);
-    $data = htmlspecialchars($data);
+function filter($data){
+    if (is_string($data)) {
+        $data = trim($data);
+        $data = htmlspecialchars($data);
+    }elseif(is_array($data)){
+        foreach($data as $key => $val){
+            $data[$key] = trim($val);
+            $data[$key] = htmlspecialchars($val);  
+        }
+    }
     return $data;
 }
