d9384b8cd55aa62168e7cb1714606dba234332b2
ghamza-dev <elgassaihamzam54@gmail.com>
2022-02-20 19:39:46 +0100

    Create alert view

--- app/views/inc/alert.php
+++ app/views/inc/alert.php
@@ -1,5 +1,5 @@
-<?php if(!empty($this->data['alert'])): ?>
-    <div class="<?= $this->data['err'] ? 'alert alert-danger' : 'alert alert-success' ?>" role="alert">
-        <?= $this->data['alert'] ?>
+<?php if(!empty($data['alert'])): ?>
+    <div class="<?= $data['err'] ? 'alert alert-danger' : 'alert alert-success' ?>" role="alert">
+        <?= $data['alert'] ?>
     </div>
 <?php endif; ?>
