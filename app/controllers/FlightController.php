524ea955084a02d807e0985fd2e004309f557755
ghamza-dev <elgassaihamzam54@gmail.com>
2022-02-20 20:15:26 +0100

    Create add flight

--- app/controllers/FlightController.php
+++ app/controllers/FlightController.php
@@ -30,6 +30,56 @@ class FlightController extends Controller{
         $this->getFlights();
     }
 
+    public function loadAddForm(){
+        $this->data['title'] = 'Add flight';
+        $this->view('admin.views/add-flight',$this->data);
+    }
+
+    public function addFlight($params = null){
+        if (!($_SERVER['REQUEST_METHOD'] === 'POST')) {
+            $this->loadAddForm();
+            return;
+        }
+
+        if (isset($params['addflight'])) {
+            $error = [];
+
+            if (empty($params['from'])) 
+                $error['from_err'] = '*This feild is required'; 
+
+            if (empty($params['to'])) 
+                $error['to_err'] = '*This feild is required';
+
+            if (empty($params['depart'])) 
+                $error['depart_err'] = '*This feild is required';
+            
+
+            if(empty($params['arrival'])) 
+                $error['arrival_err'] = '*This feild is required';
+            
+
+            if (!filter_var($params['price'],FILTER_VALIDATE_FLOAT)) 
+                $error['price_err'] = '*Please provide a valide number';
+            
+            if (!filter_var($params['seats'],FILTER_VALIDATE_INT)) 
+                $error['seats_err'] = '*Please provide a valide number';
+            
+
+            if (!empty($error)) {
+                $this->data['error'] = $error;
+                $this->loadAddForm();
+                return;
+            }
+            
+            $data = [$params['from'],$params['to'],$params['depart'],$params['arrival'],$params['price'],$params['seats'],null];
+           
+            if ($this->model->insertFlight($data)) {
+                $this->data['alert'] = 'Flight added successfully';
+                $this->loadAddForm();
+            }else dump('Somthig went wrong');
+        }
+    }
+
     public function loadUpdateForm($id){
         $flight = $this->model->selectFlightById($id);
         $this->data['flight'] = $flight;
---
+++ app/views/admin.views/add-flight.php
@@ -0,0 +1,47 @@
+<?php
+
+$error = $data['error'] ?? null;
+$values = $data['flight'] ?? null;
+
+// dump($error);
+// dump($values);
+
+?>
+
+<?php require_once VIEWS.DS.'inc/header.php' ?>
+    <h1>Add Flight</h1>
+    <form action="<?= URLROOT ?>flight/addFlight" method="post">
+        <div class="mb-3">
+            <label for="from" class="form-label">From</label>
+            <input value="<?= $values['aFrom'] ?? '' ?>" name="from" type="text" class="form-control" id="from" aria-describedby="">
+            <small class="text-danger"><?= $error['from_err'] ?? '' ?></small>
+        </div>
+        <div class="mb-3">
+            <label for="to" class="form-label">To</label>
+            <input value="<?= $values['aTo'] ?? '' ?>" name="to" type="text" class="form-control" id="to">
+            <small class="text-danger"><?= $error['to_err'] ?? '' ?></small>
+        </div>
+        <div class="mb-3">
+            <label for="departtime" class="form-label">Depart time</label>
+            <input value="<?= $values['departTime'] ? explode(' ',$values['departTime'])[0].'T'.explode(' ',$values['departTime'])[1] : '' ?>" name="depart" type="datetime-local" id="departtime">
+            <small class="text-danger"><?= $error['depart_err'] ?? '' ?></small>
+        </div>
+        <div class="mb-3">
+            <label for="arrivaltime" class="form-label">Arrival time</label>
+            <input value="<?= $values['arrivalTime'] ? explode(' ',$values['arrivalTime'])[0].'T'.explode(' ',$values['departTime'])[1] : '' ?>" name="arrival" type="datetime-local" id="arrivaltime">
+            <small class="text-danger"><?= $error['arrival_err'] ?? '' ?></small>
+        </div>
+        <div class="mb-3">
+            <label for="price" class="form-label">Price</label>
+            <input value="<?= $values['price'] ?? '' ?>" name="price" type="number" id="price" step="0.01">
+            <small class="text-danger"><?= $error['price_err'] ?? '' ?></small>
+        </div>
+        <div class="mb-3">
+            <label for="seats" class="form-label">Seats</label>
+            <input value="<?= $values['nbrPlaces'] ?? '' ?>" name="seats" type="number" id="seats">
+            <small class="text-danger"><?= $error['seats_err'] ?? '' ?></small>
+        </div>
+
+        <button type="submit" name="addflight" class="btn btn-primary">Submit</button>
+    </form>
+<?php require_once VIEWS.DS.'inc/footer.php' ?>
