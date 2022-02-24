7ad1ba4f21b0866580dabcf5237d67f8c400edbf
ghamza-dev <elgassaihamzam54@gmail.com>
2022-02-24 23:18:13 +0100

    Create authentication class

---
+++ core/Auth.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class Auth{
+    
+    public function __construct($id,$role = null){
+        $_SESSION['logged'] = true;
+        $_SESSION['id'] = $id;
+        $_SESSION['role'] = $role;
+    }
+
+    private static function redirect($location){
+        header('location:'.URLROOT.$location);
+        exit;
+    }
+
+    private static function _404(){
+        header("HTTP/1.0 404 Not Found");exit;
+    }
+
+    public static function check($role = false){
+        if (!isset($_SESSION['logged']) || !$_SESSION['logged']){
+            self::redirect("user/login");
+        }
+
+        if ($role && $_SESSION['role'] != $role) {
+            self::_404();
+        }
+    }
+
+}
