8c4c501c4a301681d1aaedf178654d0a56e02fbc
ghamza-dev <elgassaihamzam54@gmail.com>
2022-02-25 10:59:03 +0100

    Fixe alert ui

--- app/views/inc/alert.php
+++ app/views/inc/alert.php
@@ -1,5 +1,5 @@
 <?php if(!empty($data['alert'])): ?>
-    <div class="<?= $data['err'] ? 'alert alert-danger' : 'alert alert-success' ?>" role="alert">
+    <div class="<?= $data['err'] ? 'alert alert-danger' : 'alert alert-success' ?> mb-0" role="alert">
         <?= $data['alert'] ?>
     </div>
 <?php endif; ?>
