4d16e80ffa10a5ce91375554dd66e333f29540c7
ghamza-dev <elgassaihamzam54@gmail.com>
2022-02-28 15:34:44 +0100

    Production configuration

--- config/config.php
+++ config/config.php
@@ -22,13 +22,13 @@
    define('UPLOADS',ROOT.DS.'uploads');   
 
    // Database params
-   define('DB_NAME','flightlight');
-   define('DB_USER','root');
-   define('DB_HOST','localhost');
-   define('DB_PASSWD','');
+   define('DB_NAME','sql11475468');
+   define('DB_USER','sql11475468');
+   define('DB_HOST','sql11.freesqldatabase.com');
+   define('DB_PASSWD','1VUANpUEMw');
 
    // URL ROOT
-   define('URLROOT', 'http://localhost/flightline/');
+   define('URLROOT', 'https://flightline.herokuapp.com/');
 
    // Images url
    define('IMAGES', URLROOT.'/public/images');
